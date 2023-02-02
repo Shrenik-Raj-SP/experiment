52845137a3df08770a87f9a086dbdd8fc086f0dc
Shrenik-Raj-SP <32899096+Shrenik-Raj-SP@users.noreply.github.com>
2023-02-02 10:06:04 +0530

    Update runPowerShellScript.groovy

--- vars/runPowerShellScript.groovy
+++ vars/runPowerShellScript.groovy
@@ -1,6 +1,6 @@
 def call(String scriptName) {
   //def scriptPath = "${libraryResource('script/windows')}/${scriptName}"
-//  def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name}"  
-  def scriptPath = libraryResource "script/windows/hello.ps1"
+
+  def scriptPath = libraryResource "script/windows/${scriptName}"
   bat "powershell -File ${scriptPath}"
 }
